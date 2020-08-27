c5d9bfd8ed52bdfba77b46e77aeeccaff54ca6bd
Christian Kanangila <passyweb@hotmail.com>
2020-08-27 22:05:01 +0200

    to do list

---
+++ ToDoListExemple/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoListExemple.Models;
+
+namespace ToDoListExemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var todos = new List<ToDoItem>
+            {
+                new ToDoItem(1, "Etudier python"),
+                new ToDoItem(2, "Faire du sport")
+            };
+
+
+
+            var todoList = new ToDoList("Hertzy", todos);
+
+            var newId = todoList.AddTodo("Courir");
+            var todo = todoList.GetById(newId);
+            Console.WriteLine(todo);
+
+            var idList = todos
+                .Select(t => t.Id)
+                .Max();
+
+            Console.WriteLine(idList);
+        }
+    }
+}
